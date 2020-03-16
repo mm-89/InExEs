5eeee182e8556a5d51003a9cf2c85b327405dedf
marro.michele89@libero.it <abc@example.com>
2020-03-16 11:04:52 +0100

    fix issue beta coef+ random diff ray

--- math_refl_diff.py
+++ math_refl_diff.py
@@ -1,40 +1,100 @@
 from math import *
 import numpy as np
+import random
 
 def polar_transform(comp):
-    if(comp[0]==0.):
-        phi = pi/2.
-    else:
-        phi = atan(comp[1]/comp[0])
-    if(comp[2]==0.):
-        theta = pi/2.
-    else:
-        theta = acos(  comp[2]/(comp[0]*comp[0] + comp[1]*comp[1] + comp[2]*comp[2])**0.5  )
+
+    #compute theta
+    theta = acos(comp[2])
+
+    #compute phi
+    phi = np.arctan2(comp[1], comp[0])
+
     return np.array([theta, phi])
 
+#old code (it might be useful in the future)
+"""
 def compute_diff_radiation(N, theta, phi):
+
     inter_t = pi/(2*N)
     inter_p = 2*pi/N
 
     res = []
 
-    x = []
-    y = []
-    z = []
-
-    for i in range(N - 1):          #this is theta
+    for i in range(N):          #this is theta
         for j in range(N):      #this is alpha
-            x.append(sin(pi/2 - i*inter_t)*cos(j*inter_p)*sin(phi) - \
-                    (sin(pi/2 - i*inter_t)*cos(theta)*sin(j*inter_p) - \
-                        cos(pi/2 - i*inter_t)*sin(theta))*cos(phi))
-            y.append(sin(pi/2 - i*inter_t)*cos(j*inter_p)*cos(phi) + \
-                    (sin(pi/2 - i*inter_t)*cos(theta)*sin(j*inter_p) - \
-                        cos(pi/2 - i*inter_t)*sin(theta))*sin(phi))
-            z.append(sin(pi/2 - i*inter_t)*sin(theta)*sin(j*inter_p) + \
-                        cos(pi/2 - i*inter_t)*cos(theta))
+            x = sin(i*inter_t)*cos(j*inter_p)*sin(phi) - \
+                    (sin(i*inter_t)*cos(theta)*sin(j*inter_p) - \
+                        cos(i*inter_t)*sin(theta))*cos(phi)
 
-    for r in range(N*(N - 1)):
-        tmp = np.array([ x[r], y[r], z[r] ])
-        res.append(tmp)
+            y = sin(i*inter_t)*cos(j*inter_p)*cos(phi) + \
+                    (sin(i*inter_t)*cos(theta)*sin(j*inter_p) - \
+                        cos(i*inter_t)*sin(theta))*sin(phi)
+
+            z = sin(i*inter_t)*sin(theta)*sin(j*inter_p) + \
+                        cos(i*inter_t)*cos(theta)
+
+            res.append(np.array([ x, y, z]))
 
     return res
+"""
+
+def matrix_rotation(theta, phi):
+    #first rotation in xy axis 
+    first = np.array([[cos(phi), -sin(phi), 0.],[sin(phi), cos(phi), 0.],[0., 0., 1.]])
+    
+    #second rotation in xz axis
+    second = np.array([[cos(theta), 0., sin(theta)],[0., 1., 0.],[-sin(theta), 0., cos(theta)]])
+
+    return np.dot(first, second)
+
+#uniform points generation in a hemisphere
+def point_hemisphere_uniform(N):
+
+    inter_t = pi/(2*N)
+    inter_p = 2*pi/N
+
+    res = []
+
+    for i in range(N):          #this is theta
+        for j in range(N - 1):      #this is phi
+            x = sin(i*inter_t)*cos(j*inter_p)
+
+            y = sin(i*inter_t)*sin(j*inter_p)
+
+            z = cos(i*inter_t)
+
+            res.append(np.array([x, y, z]))
+
+    return res
+
+#random points generation in a hemisphere
+def point_hemisphere_random(N):
+
+    res = []
+
+    for i in range(N*(N - 1)):  #just to have same dimensionality of uniform one
+        phi = 2*pi*random.uniform(0,1)
+        theta = np.arccos(random.uniform(0,1))
+
+        x = sin(theta)*cos(phi)
+        y = sin(theta)*sin(phi)
+        z = (cos(theta))
+    
+        res.append(np.array([x, y, z]))
+
+    return res
+
+def make_rays_in_a_hemisphere(N, theta, phi, random=True):
+    if(random):
+        my_points = point_hemisphere_random(N)
+    else:
+        my_points = point_hemisphere_uniform(N)
+  
+    my_points_new = []
+
+    for i in my_points:
+        my_points_new.append(np.dot(matrix_rotation(theta, phi), i))
+
+    return my_points_new
+    
--- posture.py
+++ posture.py
@@ -1,8 +1,10 @@
 import trimesh as tm
 import math_refl_diff as mrd
-import numpy as np #not necessary
+import numpy as np
 
-class posture:
+np.set_printoptions(suppress=True) #I dunno if now it is necessary
+
+class Posture:
 
 	def __init__(self, my_file):
 		self.my_file = tm.load(my_file)
@@ -13,17 +15,18 @@ class posture:
 		self.my_file.remove_unreferenced_vertices()
 
 		normals = self.my_file.face_normals
-		new_normals = []
+		angles_normals = []
 		for comp in normals:
-			new_normals.append(mrd.polar_transform(comp))
+			angles_normals.append(mrd.polar_transform(comp))
 		
-		self.new_normals = new_normals
+		self.angles_normals = angles_normals
+		self.normals_minimized = self.my_file.face_normals*0.01
 
- 
+	@property
 	def get_posture(self):
 		return self.my_file
 
-
+	@property
 	def get_normals(self):
 		return self.my_file.face_normals
 
@@ -35,41 +38,62 @@ class posture:
 	def get_total_area(self):
 		return self.my_file.area
 
-
-	def get_normals_min(self, fact=1000.):
-		normals_minimized = []
-		fact = float(fact)
-		for comp in self.my_file.face_normals:
-			normals_minimized.append(comp/fact)
-		return normals_minimized
+	@property
+	def get_normals_minimized(self):
+		return self.normals_minimized
 
 
-	def compute_beta(self, N=20):
-		ray_ori_all = self.my_file.triangles_center + \
- 		self.my_file.face_normals/1000.
+	def compute_beta(self, N, random=True):
+		ray_ori_all = self.my_file.triangles_center + self.my_file.face_normals
 
 		ray_dir = []
-
-		j = 0
+		angles = self.angles_normals
+		
 		beta = []
 
-		for t in ray_ori_all:
+		if(random):
+			for counter, t in enumerate(ray_ori_all):
 
-			ray_ori = []
-			for i in range(N*(N - 1)):
+				ray_ori = []
 
-				ray_ori.append(t)
-				ray_ori=[t for i in range(N*(N - 1))]
-				ray_dir =mrd.compute_diff_radiation(N, self.new_normals[j][0], \
-													self.new_normals[j][1])
+				for i in range(N*(N - 1)):
 
-				res = self.my_file.ray.intersects_any(ray_origins=ray_ori, \
-															ray_directions=ray_dir)
+					ray_ori.append(t)
+					ray_ori = [t for i in range(N*(N - 1))]
+					ray_dir = mrd.make_rays_in_a_hemisphere(N, angles[counter][0], 
+																angles[counter][1], random=True)
 
-			cpt_false = len(np.nonzero(~res)[0])
-			beta.append(cpt_false/(N*(N - 1)))
-			print(j)
-			j += 1
+					res = self.my_file.ray.intersects_any(ray_origins=ray_ori,
+														ray_directions=ray_dir)
+				cpt_false = np.nonzero(~res)[0]
+				beta.append(len(cpt_false)/(N*(N - 1)))
+
+				print("Computing beta ... ", 
+					round(counter/len(ray_ori_all)*100,1), 
+					" percent complete", end="\r")
+
+		else:
+
+			for counter, t in enumerate(ray_ori_all):
+
+				ray_ori = []
+				for i in range(N*(N - 1)):
+
+					ray_ori.append(t)
+					ray_ori = [t for i in range(N*(N - 1))]
+					ray_dir = mrd.make_rays_in_a_hemisphere(N, angles[counter][0], angles[counter][1], random=random)
+
+					res = self.my_file.ray.intersects_any(ray_origins=ray_ori, \
+																ray_directions=ray_dir)
+			
+				cpt_false = np.nonzero(~res)[0]
+				tmp = len(cpt_false)
+				beta.append(tmp/(N*(N - 1)))
+
+				print("Computing beta ... ", 
+					round(counter/len(ray_ori_all)*100,1), 
+					" percent complete", end="\r")
+
 		return beta
 
 
@@ -87,8 +111,10 @@ class posture:
 
 
 	def get_angles_from_normals(self):
-		return self.new_normals
+		return self.angles_normals
 
+	def set_normals_minimized(self, fact=0.01):
+		return self.face_normals*fact
 
 	def show_posture(self):
 		self.my_file.show()
