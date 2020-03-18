c8267470a38e32cf83ebdef30697edabe693e985
marro.michele89@libero.it <abc@example.com>
2020-03-18 14:38:51 +0100

    show_one_timestep add in Simulation class

--- main.py
+++ main.py
@@ -20,75 +20,53 @@ extension = "ply"
 #define and charge a posture
 posture = ps.Posture(my_file)
 
-#need normals
-normals = posture.get_normals
-
-#minimized them to avoid extra intersections
-normals_minimized = posture.get_normals_minimized
-#--------------------------------------------
-
 #SUNRAYS SETUP ----------------------------------
 #set the sun ray direction (one step for now)
-sun_ray = srd.Sun_ray_direction(second=400)
-sun_direction = sun_ray.get_sun_direction()
+sun_ray = srd.Sun_ray_direction()
+sun_direction = sun_ray.get_sun_direction(day=1, second=43200)
 #--------------------------------------------
 
 
 #SHADOW MAPPING ----------------------------------
 #look for intersections
-ray_origins = posture.get_vertices_barycenter() + normals_minimized
-ray_direction = [sun_direction for i in range(len(ray_origins))]
+ray_origins = posture.get_vertices_barycenter + posture.normals_minimized
+#ray_direction = [sun_direction for i in range(len(ray_origins))]
 
 #just to check
-if not len(ray_origins)==len(ray_direction):
-	print("Some problems occured")
+#if not len(ray_origins)==len(ray_direction):
+#	print("Some problems occured")
 
-#try to evaluate intersections
+#new_origins = []
+#for comp in ray_origins:
+#	if(np.dot(comp, sun_direction) > 0 and np.dot(comp, sun_direction) <= 1):
+#		new_origins.append(comp)
+
+#print("old lenght: ", len(ray_origins), " new lenght: ", len(new_origins))
+
+ray_direction = [sun_direction for i in range(len(ray_origins))]
+
+#OLD VERSION
+#try to evaluate intersections 
+#inf = posture.get_posture.ray.intersects_any(ray_origins=ray_origins, ray_directions=ray_direction)
+
+#print("old lenght: ", len(new_origins), " new lenght: ", len(ray_direction))
+
+#NEW VERSION
 inf = posture.get_posture.ray.intersects_any(ray_origins=ray_origins, ray_directions=ray_direction)
 
 #take only non-zero components (non-zero=not hit)
 face_nohit = np.nonzero(~inf)[0]
 
-#to highlithg illuminated comparet to in shadow faces
-black_col = [0, 0, 0]
-white_col = [255, 255, 255]
 
-col_ver = []
-for comp in inf:
-	if not comp:
-		col_ver.append(white_col)
-	else:
-		col_ver.append(black_col)
-
-if(len(col_ver)==len(posture.get_faces())):
-	print("Program is working")
 
 #try to re-write a mesh
-my_new_mesh = tm.Trimesh(vertices=posture.get_vertices(), faces=posture.get_faces(), \
-				process=True, face_colors=col_ver)
+my_new_mesh = tm.Trimesh(vertices=posture.get_vertices, 
+						faces=posture.get_faces, 
+						process=True, 
+						#face_colors=col_ver
+						)
 
+#IMPORTANT TO IMPLEMENTATE
 #to export a mesh - it works
-tm.exchange.export.export_mesh(my_new_mesh, file_out + "." + extension)
+#tm.exchange.export.export_mesh(my_new_mesh, file_out + "." + extension)
 #--------------------------------------------
-
-#BETA COEFICIENT ----------------------------------
-#try to compute beta coefficient
-start = time.time()
-posture.compute_beta(N=5, random=False)
-print(time.time() - start)
-#--------------------------------------------
-
-
-#DISPLAY 3D VISUALISATION ----------------------------------
-"""
-#visualize current mesh
-ray_visualize = tm.load_path(np.hstack((
-        ray_origins[100],
-        ray_origins[100] + ray_direction[100])).reshape(-1, 2, 3))
-
-scene = tm.Scene([
-        my_new_mesh,
-        ray_visualize])
-
-scene.show()
-"""
--- simulation.py
+++ simulation.py
@@ -1,49 +1,61 @@
 import posture as ps
 import sun_ray_direction as srd
+
+import trimesh as tm
+import numpy as np
 import datetime
 import time
 
 class Simulation:
 
 	def __init__(self, 
-				start_date, 
-				end_date, 
+                start_date, 
+		    	end_date, 
 				timestep, 
 				posture, 
-				source_light
+				source_light,
+				start_angle
 				):
 
-		self.start_date = datetime.datetime(start_date[0],
-											start_date[1],
-											start_date[2],
-											start_date[3],
-											start_date[4],
-											start_date[5])
-		self.end_date = datetime.datetime(end_date[0],
-											end_date[1],
-											end_date[2],
-											end_date[3],
-											end_date[4],
-											end_date[5])
+	    self.start_date = datetime.datetime(start_date[0],  
+                                                start_date[1],
+												start_date[2],
+												start_date[3],
+												start_date[4],
+												start_date[5])
+	    self.end_date = datetime.datetime(end_date[0],									
+	    										end_date[1],
+												end_date[2],
+												end_date[3],
+												end_date[4],
+												end_date[5])
 
-		self.day_of_beginning = (datetime.date(start_date[0],
+	    self.day_of_beginning = (datetime.date(start_date[0],
 												start_date[1],
-												start_date[2]) - \
-									datetime.date(start_date[0], 
-													1,
-													1)).days
+		        								start_date[2]) - \
+												datetime.date(start_date[0], 							
+												1,
+												1)).days
 
-		self.start_second = start_date[5]
-		self.timestep = timestep
-		self.posture = ps.Posture(posture)
-		self.source_light = source_light 
+	    self.start_second = start_date[5]
+	    self.timestep = timestep
+	    self.posture = ps.Posture(posture)
+	    self.source_light = source_light 
+	    self.start_angle = start_angle
 
-		#some information for users
-		print("start date is: ", self.start_date.strftime("%b %d %Y %H:%M:%S"))
-		print("end date is:   ", self.end_date.strftime("%b %d %Y %H:%M:%S"))
-		print("Posture that has to be simulated is: ", posture)
+	    self.ray_origins = self.posture.get_vertices_barycenter + \
+	    					self.posture.get_normals_minimized
+
 
 	def make_simulation(self):
+
+
+		#some information for users
+		print("")
+		print("start date is: ", self.start_date.strftime("%b %d %Y %H:%M:%S"))
+		print("end date is:   ", self.end_date.strftime("%b %d %Y %H:%M:%S"))
+		print("Posture that has to be simulated is: ", self.posture)
+		print("")
 
 		#reference data
 		current_data = self.start_date
@@ -60,16 +72,15 @@ class Simulation:
 			#compute source rays direction
 			ray_source_direction = self.source_light.get_sun_direction(current_day, current_second)
 
-			ray_origins = self.posture.get_vertices_barycenter + self.posture.get_normals_minimized
-			ray_direction = [ray_source_direction for i in range(len(ray_origins))]
+			ray_direction = [ray_source_direction for i in range(len(self.ray_origins))]
 
 			#just to check
-			if not len(ray_origins)==len(ray_direction):
+			if not len(self.ray_origins)==len(ray_direction):
 				print("Some problems occured")
 				break
 
 			#ray_tracing
-			inf = self.posture.get_posture.ray.intersects_any(ray_origins=ray_origins, 
+			inf = self.posture.get_posture.ray.intersects_any(ray_origins=self.ray_origins, 
 														ray_directions=ray_direction)
 
 			current_data += datetime.timedelta(seconds=self.timestep)
@@ -79,8 +90,63 @@ class Simulation:
 				current_second = 86400 - current_second
 				current_day = 1
 
-			print("day: ", current_day)
-			print("second: ", current_second)
-
 		print("Total time of simulation: ", time.time() - start, " seconds")
-			#print(current_data.time())
+
+
+	def show_one_timestep(self, date, show_result=True):
+		#this just to visualize
+		date_to_vis = datetime.datetime(date[0],
+											date[1],
+											date[2],
+											date[3],
+											date[4],
+											date[5])
+
+		print("You are visualizing: ", date_to_vis.strftime("%b %d %Y %H:%M:%S"))
+
+		number_of_days = (datetime.date(date[0],
+											date[1],
+											date[2]) - \
+											datetime.date(date[0], 1, 1)).days
+
+		#make rays of sun (direction)
+		ray_source_direction = self.source_light.get_sun_direction(number_of_days, date[5])
+		ray_direction = [ray_source_direction for i in range(len(self.ray_origins))]
+
+		#rays tracing
+		inf = self.posture.get_posture.ray.intersects_any(ray_origins=self.ray_origins, 
+														ray_directions=ray_direction)
+
+		#take only non-zero components (non-zero=not hit)
+		face_nohit = np.nonzero(~inf)[0]
+
+		#to highlithg illuminated comparet to in shadow faces
+		black_col = [0, 0, 0]
+		white_col = [255, 255, 255]
+
+		#set white if the component is false (no hit)
+		#or set black if the component is true (hit some faces)
+		col_ver = []
+		for comp in inf:
+			if not comp:
+				col_ver.append(white_col)
+			else:
+				col_ver.append(black_col)
+
+		#try to re-write a mesh
+		my_new_mesh = tm.Trimesh(vertices=self.posture.get_vertices, 
+								faces=self.posture.get_faces,
+								process=True, 
+								face_colors=col_ver)
+
+		#if you want to visualize a ray
+		#ray_visualize = tm.load_path(np.hstack((
+		#ray_origins[100],
+		#ray_origins[100] + ray_direction[100])).reshape(-1, 2, 3))
+
+		scene = tm.Scene([
+						my_new_mesh,
+						#ray_visualize
+						])
+
+		if(show_result): scene.show()
