2b4108e892856c9642b378d115917fe36662f83d
Michele Marro <michele.marro@unige.ch>
2020-03-30 16:42:48 +0200

    simulation with reading data input

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import trimesh as tm
 my_data_file = "input/irradiance_2009.csv"
 
 #my_posture_file = "postures/head_high_res/head.ply"
-my_posture_file = "face_final.ply"
+my_posture_file = "male_eyesballs_reduced_faces.ply"
 
 output_name = "data"
 
@@ -28,32 +28,32 @@ timestep = 60.
 
 #GEO PARAMETERS----------------------------
 
-latitude = 8.1
+latitude = 46.8
 
 #POSTURE PARAMETERS--------------------------
 #need start angle
 
 start_angle_theta = 0.
-start_angle_phi = 180.
+start_angle_phi = 0.
 
 #DATA PARAMETERS------------------------------
 #set start date
 
-s_year = 2010
-s_month = 7
-s_day = 11
-s_hour = 7
-s_minute = 0
+s_year = 2009
+s_month = 1
+s_day = 1
+s_hour = 12
+s_minute = 1
 s_second = 0
 
 #----------------------------------------
 #set end date
 
-e_year = 2010
-e_month = 7
-e_day = 11
-e_hour = 17
-e_minute = 0
+e_year = 2009
+e_month = 1
+e_day = 1
+e_hour = 12
+e_minute = 40
 e_second = 0
 
 #BETA COEFFICIENT
@@ -71,19 +71,20 @@ end_date = [e_year, e_month, e_day, e_hour, e_minute, e_second]
 
 posture = ps.Posture(my_posture_file,N)
 
-sun_ray_source = srd.Sun_ray_direction(read_data=False,	data_path=my_data_file,
-										latitude=latitude)
+#sun_ray_source = srd.Sun_ray_direction(latitude=latitude)
 
 #-----------------------------------------
 
-my_simulation = sim.Simulation(	start_date, 
+my_simulation = sim.Simulation(start_date, 
 								end_date, 
 								timestep, 
 								posture,
-								sun_ray_source,
 								start_angle_theta,
 								start_angle_phi,
-								output_name
+								output_name,
+								latitude=latitude,
+								read_data=False,
+								data_path=my_data_file,
 								)
 
 #to visualize a particular timestep
