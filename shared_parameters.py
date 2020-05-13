a12ae80f34649d959bca02e7148d9b19f57c8f4a
Michele Marro <michele.marro@unige.ch>
2020-05-13 08:56:58 +0200

    updated comments

--- shared_parameters.py
+++ shared_parameters.py
@@ -5,19 +5,10 @@
 #N: number of points generated on the hemisphere
 #for beta coefficients computation
 
-#hemispherical random generator: beta coefficients
-#can be computed via random way (Monte Carlo) or
-#uniformilly distribuited [the second converges
-#much faster than first]
-
 #normalizazion factor: to set rays origins different
 #from faces' centers. Speed of beta coefficients 
 #computed change with this parameter, as well as 
 #the precision 
-
-#threshold is the limit used in math_refl_diff
-#to set zero a value under the value of
-#threshold itself
 
 N = 50000
 normalization_factor = 1e-6
