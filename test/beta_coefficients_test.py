d0dbe037d05b75f9c258e3d77b562c726b90a0f5
Michele Marro <michele.marro@unige.ch>
2020-05-14 14:16:36 +0200

    add test for tetrahedron

--- test/beta_coefficients_test.py
+++ test/beta_coefficients_test.py
@@ -5,7 +5,7 @@ sys.path.insert(0, '../')
 
 import trimesh as tm
 from scipy.integrate import quad
-from math import cos, sin, pi
+from math import cos, sin, atan, pi
 import random
 
 random.seed(12)
@@ -21,14 +21,14 @@ my_tetrahedron = "postures_test/tetrahedron_test.ply"
 my_mesh_cube = tm.load(my_cube)
 my_mesh_tetrahedron = tm.load(my_tetrahedron)
 
-# usual function for beta coefficient for zenith part ---
-def integral_arg_2(theta, phi, theta_0, phi_0):
-    return (sin(theta)*cos(phi)*sin(theta_0)*cos(phi_0) + \
-            sin(theta)*sin(phi)*sin(theta_0)*sin(phi_0) + \
-            cos(theta)*cos(theta_0))*sin(theta)
+#simple integral function for tetrahedron
+#it comes from integral of labert law with z=sqrt(2)x
+def integral_tetr(x):
+    return (sin(atan(1/((2**0.5) *sin(x)))))**2
 
-def integral_arg(theta, theta_0):
-    return cos(theta_0 -theta)*sin(theta)
+#simple integral function for cube
+def integral_cube(x, theta_0):
+    return cos(theta_0 - x)*sin(x)
 #--------------------------------------------------------
 
 class Test_Beta_Coefficients(unittest.TestCase):
@@ -41,11 +41,12 @@ class Test_Beta_Coefficients(unittest.TestCase):
                                 my_mesh_cube.triangles_center + \
                                 my_mesh_cube.face_normals*sp.normalization_factor)
 
-        # parameter - analitical
-        self.top_diff, self.top_diff_err = quad(integral_arg, 0, pi/2., args=(0.))
-        self.bot_refl, self.bottom_err = quad(integral_arg, pi/2., pi, args=(pi))
+        #diff and refl parameter - analitical
+        self.top_diff, self.top_diff_err = quad(integral_cube, 0, pi/2., args=(0.))
+        self.bot_refl, self.bottom_err = quad(integral_cube, pi/2., pi, args=(pi))
 
-        self.lat, self.lat_err = quad(integral_arg, 0, pi/2., args=(0.))
+        #lateral parameter - analitical
+        self.lat, self.lat_err = quad(integral_cube, 0, pi/2., args=(0.))
         #----------------------------------------------------------
 
         #for the tetrahedron mesh------------------------------------------
@@ -56,8 +57,18 @@ class Test_Beta_Coefficients(unittest.TestCase):
                                 my_mesh_tetrahedron.face_normals*sp.normalization_factor)
 
         #diff parameter - analitical
-        self.top_diff_tr = pi*quad(integral_arg, 0, pi/2., args=(0.))[0]
+        #divide the domain in 1/4 of sphere + 1/8 of sphere
+        self.top_diff_tr = quad(integral_tetr, 0, pi)[0]/2. + \
+                                pi*quad(integral_cube, 0, pi/2., args=(0.))[0]
 
+        self.bot_diff_tr = pi/2. - quad(integral_tetr, 0, pi)[0]/2.
+
+        #refl parameter - analitical
+        self.top_refl_tr = pi/2. - quad(integral_tetr, 0, pi)[0]/2.
+
+        self.bot_refl_tr = quad(integral_tetr, 0, pi)[0]/2. + \
+                                pi*quad(integral_cube, pi/2., pi, args=(pi))[0]
+        
         #----------------------------------------------------------
 
     def test_compute_beta_coefficient_cube(self):
@@ -172,12 +183,48 @@ class Test_Beta_Coefficients(unittest.TestCase):
         self.assertAlmostEqual(self.beta_coeff_cube[11, 1], 
                                 pi*self.lat, 
                                 delta=self.beta_coeff_cube[11, 3])
-    """
+
+
     def test_compute_beta_coefficient_tetrahedron(self):
         # surface 1 diff - top
         self.assertAlmostEqual(self.beta_coeff_tetr[0, 0], 
-                                pi*self.top_diff_tr, 
+                                self.top_diff_tr, 
                                 delta=10*self.beta_coeff_tetr[0, 2])
-    """
+
+        # surface 2 diff - bot
+        self.assertAlmostEqual(self.beta_coeff_tetr[1, 0], 
+                                self.bot_diff_tr, 
+                                delta=10*self.beta_coeff_tetr[1, 2])
+
+        # surface 3 diff - top
+        self.assertAlmostEqual(self.beta_coeff_tetr[2, 0], 
+                                self.top_diff_tr, 
+                                delta=10*self.beta_coeff_tetr[2, 2])
+
+        # surface 4 diff - bot
+        self.assertAlmostEqual(self.beta_coeff_tetr[3, 0], 
+                                self.bot_diff_tr, 
+                                delta=10*self.beta_coeff_tetr[3, 2])
+
+        # surface 1 refl - top
+        self.assertAlmostEqual(self.beta_coeff_tetr[0, 1], 
+                                self.top_refl_tr, 
+                                delta=10*self.beta_coeff_tetr[0, 2])
+
+        # surface 2 refl - bot
+        self.assertAlmostEqual(self.beta_coeff_tetr[1, 1], 
+                                self.bot_refl_tr, 
+                                delta=10*self.beta_coeff_tetr[1, 3])
+
+        # surface 3 refl - top
+        self.assertAlmostEqual(self.beta_coeff_tetr[2, 1], 
+                                self.top_refl_tr, 
+                                delta=10*self.beta_coeff_tetr[2, 3])
+
+        # surface 4 refl - bot
+        self.assertAlmostEqual(self.beta_coeff_tetr[3, 1], 
+                                self.bot_refl_tr, 
+                                delta=10*self.beta_coeff_tetr[3, 3])
+
 if __name__ == '__main__':
     unittest.main()
